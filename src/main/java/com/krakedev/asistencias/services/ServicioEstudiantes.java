d177da3c0e01a704729d4bfca090a611797b5528
GaloRamiro <g_cisneros_23@hotmail.es>
2026-09-25 14:45:31 -0500

    feat: clase servicios

---
+++ src/main/java/com/krakedev/asistencias/services/ServicioEstudiantes.java
@@ -0,0 +1,14 @@
+package com.krakedev.asistencias.services;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.krakedev.asistencias.entidades.Estudiante;
+
+@Service
+public class ServicioEstudiantes {
+
+	private ArrayList<Estudiante> estudiantes = new ArrayList<>();
+
+}
